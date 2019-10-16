d10db31a4c96b35d8cc1b794b85cc5a533a74e88
Karolis Lesevicius <karolis.lesevicius@gmail.com>
2019-10-16 19:59:18 +0300

    Use static products instead of generating dynamically

--- routes/foodShop/index.js
+++ routes/foodShop/index.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const { NAME } = require(path.resolve(__dirname, "constants"));
 const { generateProducts } = require(path.resolve(__dirname, "utils"));
+const generatedProducts = require('./generatedProducts.json');
 
 const routes = app => {
-  app.get(`/${NAME}/products`, (req, res) => res.json(generateProducts()));
+  app.get(`/${NAME}/products`, (req, res) => res.json(generatedProducts));
 };
 
 module.exports = routes;
