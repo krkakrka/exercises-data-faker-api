087552cac0320c856b513291810fcf0dca6258ca
Karolis Lesevicius <karolis.lesevicius@gmail.com>
2019-10-28 17:37:24 +0200

    Replaced lorempixel images
    Slow

--- routes/foodShop/index.js
+++ routes/foodShop/index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const { NAME } = require(path.resolve(__dirname, "constants"));
-const { generateProducts } = require(path.resolve(__dirname, "utils"));
+// const { generateProducts } = require(path.resolve(__dirname, "utils"));
 const generatedProducts = require('./generatedProducts.json');
 
 const routes = app => {
